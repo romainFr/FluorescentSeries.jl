6fd5e3c423cc49b36719610e62850c211e614c2f
Romain Franconville <romain.franconville@gmail.com>
2017-02-09 09:22:08 -0500

    Typo

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -1,5 +1,5 @@
 ## DeltaF/F
-function deltaFF(fs::AxisArray,Fo::Array{Float64,1},B::Float64=0.0)
+function deltaFF(fs::AxisArray,Fo::Array{Float64,1},B=0.0)
     length(Fo) != size(fs)[2] ? error("Fo vector should be the same length as the number of ROIs in the series."):
     Fo = Fo.'
     newF = (fs .- Fo)./(Fo-B)
