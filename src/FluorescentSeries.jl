90110019918fdf5824abcc6471e045d966b509bc
Romain Franconville <romain.franconville@protonmail.com>
2018-01-30 16:34:52 -0500

    Only export the necessary, remove commented code

--- src/FluorescentSeries.jl
+++ src/FluorescentSeries.jl
@@ -1,21 +1,11 @@
 module FluorescentSeries
-using AxisArrays, ImageAxes#, SimpleTraits, ImageAxes
+using AxisArrays, ImageAxes
 
-#import Base: +,.+,-,.-,*,.*,/,./,.^,sin,cos,tan,asin,acos,atan,sinh,cosh,tanh,asinh,acosh,atanh,exp,log,log2,log10,sqrt,lgamma,log1p,erf,erfc
-#import Base: copy,getindex,hcat,quantile,length,setindex!,size,vcat
-# package code goes here
 include("core.jl")
 include("algorithms.jl")
 
 export
-AbstractFluorescentSerie,
 FluorescentSerie,
-times,
-fluo,
-rois,
-avgImage,
-metadata,
 deltaFF
-
 
 end # module
