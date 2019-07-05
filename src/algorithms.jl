9668bf9cb798ba6e91392a03a01f5923ea64f2b2
Romain Franconville <romain.franconville@protonmail.com>
2019-07-05 12:12:49 -0400

    Explicitly convert df/f arrays to Union{Missing,Float64}

--- src/algorithms.jl
+++ src/algorithms.jl
@@ -2,6 +2,6 @@
 function deltaFF(fs::AxisArray,Fo::Array{Float64,1},B=0.0)
     length(Fo) != size(fs)[2] ? error("Fo vector should be the same length as the number of ROIs in the series.") :
     Fo = permutedims(Fo)
-    newF = (fs .- Fo)./(Fo .-B)
+    newF = convert(Array{Union{Missing, Float64},3},((fs .- Fo)./(Fo .-B)))
     AxisArray(newF,AxisArrays.axes(fs)...)
 end
